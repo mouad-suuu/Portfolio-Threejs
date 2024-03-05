a38d2a69f88b3688ec5450f46d4d7d14ea7316e9
mouad mennioui <moad.menniui@gmail.com>
2024-03-05 18:32:43 +0100

    new check poinet

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { styles } from '../styles'; 
+import { navlinks } from '../constants';
+import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar Component</h2>
-      {/* Add your content here */}
-    </div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+
+      
+      {/* The <nav> element represents the navigation bar. */}
+      {/* className attribute dynamically applies CSS classes based on external styles and conditions. */}
+      {/* ${styles.paddingX} adds horizontal padding defined in an external stylesheet. */}
+      {/* w-full ensures the element spans the full width of its container. */}
+      {/* flex allows for flexible layout and alignment of child elements. */}
+      {/* items-center centers child elements vertically within the flex container. */}
+      {/* py-5 provides padding on the top and bottom sides. */}
+      {/* fixed positions the element relative to the viewport. */}
+      {/* top-0 fixes the element to the top of the viewport. */}
+      {/* z-20 sets the stack order of the element relative to other elements. */}
+      {/* bg-primary applies the primary background color defined in an external stylesheet. */}
+    </nav>
   );
 };
 
