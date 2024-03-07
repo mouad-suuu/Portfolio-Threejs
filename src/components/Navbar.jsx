ce69a8976641dbc9d05adfefd74d82286eb3e5ea
mouad mennioui <moad.menniui@gmail.com>
2024-03-07 11:47:44 +0100

    navigation bare step one

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-import { styles } from '../styles'; 
-import { navlinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-
-      
-      {/* The <nav> element represents the navigation bar. */}
-      {/* className attribute dynamically applies CSS classes based on external styles and conditions. */}
-      {/* ${styles.paddingX} adds horizontal padding defined in an external stylesheet. */}
-      {/* w-full ensures the element spans the full width of its container. */}
-      {/* flex allows for flexible layout and alignment of child elements. */}
-      {/* items-center centers child elements vertically within the flex container. */}
-      {/* py-5 provides padding on the top and bottom sides. */}
-      {/* fixed positions the element relative to the viewport. */}
-      {/* top-0 fixes the element to the top of the viewport. */}
-      {/* z-20 sets the stack order of the element relative to other elements. */}
-      {/* bg-primary applies the primary background color defined in an external stylesheet. */}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex item-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scroll(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 objec-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Mouad Mennioui
+          </p>
+        </Link>
+      </div>
     </nav>
   );
 };
